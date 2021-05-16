89447d67a086bb22cbe3cb0c1605ff7962200418
HeyJOe03 <jocarmi03@gmail.com>
2021-05-16 13:54:33 +0200

    added queries update

--- server/controllers/updateController.js
+++ server/controllers/updateController.js
@@ -4,41 +4,92 @@ const updateSQL = require('../db/SQL/update');
 const badQuery = {'error':'bad query'};
 const noElements = {'error':'no elements'};
 const missingFields = {'error':'missing fields'};
+const goodQuery = {'good':'query succesfull'};
 
 module.exports.motorinoelettrico = (req,res) => {
     if(checkVeicoliFields(req.body) && req.body.livelloBatteria !== undefined){
-        res.json(updateSQL.motorinoelettrico(req.body));
+        //res.json(updateSQL.motorinoelettrico(req.body));
+        [veicoliQuery,specificQuery] = updateSQL.motorinoelettrico(req.body).split(';');
+        let IDquery = `SELECT * FROM motorinoelettrico WHERE ID=${req.body.ID};`;
+        DB.query(IDquery,(err,result) => {
+            if(err) res.json(badQuery);
+            else if(result !== '[]'){
+                DB.query(veicoliQuery);
+                DB.query(specificQuery);
+                res.json(goodQuery);
+            }
+            else res.json(noElements);
+        });
     }
     else res.json(missingFields);
 }
 module.exports.auto = (req,res) => {
     if(checkVeicoliFields(req.body) && req.body.livelloBatteria !== undefined){
-        res.json(updateSQL.auto(req.body));
+        [veicoliQuery,specificQuery] = updateSQL.auto(req.body).split(';');
+        let IDquery = `SELECT * FROM auto WHERE ID=${req.body.ID};`;
+        DB.query(IDquery,(err,result) => {
+            if(err) res.json(badQuery);
+            else if(result !== '[]'){
+                DB.query(veicoliQuery);
+                DB.query(specificQuery);
+                res.json(goodQuery);
+            }
+            else res.json(noElements);
+        });
     }
     else res.json(missingFields);
 }
 module.exports.monopattino = (req,res) => {
     if(checkVeicoliFields(req.body) && req.body.batteriaMassima !== undefined){
-        res.json(updateSQL.monopattino(req.body));
+        [veicoliQuery,specificQuery] = updateSQL.monopattino(req.body).split(';');
+        let IDquery = `SELECT * FROM monopattino WHERE ID=${req.body.ID};`;
+        DB.query(IDquery,(err,result) => {
+            if(err) res.json(badQuery);
+            else if(result !== '[]'){
+                DB.query(veicoliQuery);
+                DB.query(specificQuery);
+                res.json(goodQuery);
+            }
+            else res.json(noElements);
+        });
     }
     else res.json(missingFields);
 }
 module.exports.bici = (req,res) => {
     if(checkVeicoliFields(req.body) && req.body.casco !== undefined){
-        res.json(updateSQL.bici(req.body));
+        [veicoliQuery,specificQuery] = updateSQL.bici(req.body).split(';');
+        let IDquery = `SELECT * FROM bici WHERE ID=${req.body.ID};`;
+        DB.query(IDquery,(err,result) => {
+            if(err) res.json(badQuery);
+            else if(result !== '[]'){
+                DB.query(veicoliQuery);
+                DB.query(specificQuery);
+                res.json(goodQuery);
+            }
+            else res.json(noElements);
+        });
     }
     else res.json(missingFields);
 }
 module.exports.ebike = (req,res) => {
     if(checkVeicoliFields(req.body) && req.body.casco !== undefined && req.body.caricaBatteria !== undefined){
-        res.json(updateSQL.ebike(req.body));
+        [veicoliQuery,specificQuery] = updateSQL.ebike(req.body).split(';');
+        let IDquery = `SELECT * FROM ebike WHERE ID=${req.body.ID};`;
+        DB.query(IDquery,(err,result) => {
+            if(err) res.json(badQuery);
+            else if(result !== '[]'){
+                DB.query(veicoliQuery);
+                DB.query(specificQuery);
+                res.json(goodQuery);
+            }
+            else res.json(noElements);
+        });
     }
     else res.json(missingFields);
 }
-module.exports.utente = (req,res) => {
+// module.exports.utente = (req,res) => {
 
-}
-
+// }
 
 function checkVeicoliFields({ID,stato,disponibilita,latitudine,longitudine}){
     if(ID !== undefined && stato !== undefined && disponibilita !== undefined && latitudine !== undefined && longitudine !== undefined) return true;
--- server/routes/update.js
+++ server/routes/update.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const updateControllers = require('../controllers/updateController');
 
-
 router.get('/test',(req,res) => {
     res.send('route page');
 })
-
 
 router.post('/motorinoelettrico',updateControllers.motorinoelettrico);
 router.post('/auto',updateControllers.auto);
